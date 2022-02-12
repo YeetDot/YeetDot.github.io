9fe11d35beb053794e4f53963d8820f3c849e2d1
YeetDot <randomrepl@gmail.com>
2022-02-12 16:40:34 -0500

    -

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function gotoalloysmelt() {
  window.location.href = 'https://www.alloysmelt.ga/alloysmelt';
 }
-function gotoalloysmelt() {
+function gotosecret() {
  window.location.href = 'https://www.alloysmelt.ga/secret/secret.html';
 }
